6ff5691276becbab7ad3303859f35659f72175c5
yourenoticed <yourenoticed@gmail.com>
2024-11-19 12:52:39 +0700

    added getters

--- database/requests.py
+++ database/requests.py
@@ -30,3 +30,50 @@ async def add_user(user: list) -> None:
 async def make_admin(tg_id: int) -> None:
     async with engine.begin() as conn:
         await conn.execute(insert(Admin).values([tg_id]))
+
+
+async def get_menu_items() -> list[Menu]:
+    async with session() as s:
+        result = await s.scalars(select(Menu))
+        return result.all()
+
+
+async def get_clients() -> list[Client]:
+    async with session() as s:
+        result = await s.scalars(select(Client))
+        return result.all()
+
+
+async def get_active_orders() -> list[Order]:
+    async with session() as s:
+        result = await s.scalars(select(Order).where(Order.finished_at == None))
+        return result.all()
+
+
+async def get_finished_orders() -> list[Order]:
+    async with session() as s:
+        result = await s.scalars(select(Order).where(Order.finished_at != None, Order.canceled == False))
+        return result.all()
+
+
+async def get_canceled_orders() -> list[Order]:
+    async with session() as s:
+        result = await s.scalars(select(Order).where(Order.canceled == True))
+        return result.all()
+
+
+# I think I can come up with a better solution for these two functions
+async def get_active_clients() -> list[Client]:
+    active_clients_id = {order.tg_id for order in await get_active_orders()}
+    clients = await get_clients()
+    active_clients = [
+        client for client in clients if client.tg_id in active_clients_id]
+    return active_clients
+
+
+async def get_unactive_clients() -> list[Client]:
+    active_clients_id = {order.tg_id for order in await get_active_orders()}
+    clients = await get_clients()
+    unactive_clients = [
+        client for client in clients if client.tg_id not in active_clients_id]
+    return unactive_clients
